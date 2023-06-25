72badbeb67916f94dec6c6d255169953011743d7
jminkkk <minl741@naver.com>
2023-06-25 17:35:06 +0900

    1차 페어: 지민, 현수

---
+++ bulls-and-cows-pair/src/model/Model.java
@@ -0,0 +1,10 @@
+package model;
+
+
+import java.util.List;
+
+public class Model {
+    public static List<Integer> randomNum;
+    public static List<Integer> userNum;
+    public static String result;
+}
